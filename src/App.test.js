151d1d37e9facf06f766cd4fe2107d2c3c87f0f2
Taja House <ahlliahtaja@yahoo.com>
2020-06-04 08:26:10 -0700

    Almost

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { render, waitFor, fireEvent
+import { render, waitFor, fireEvent, getByText
  } from "@testing-library/react";
 import App from "./App"
 import { fetchShow as mockFetchShow } from "./api/fetchShow";
 import {episodeFixture} from "./components/Episodes.test";
+import { act } from "react-dom/test-utils";
+import ReactDOM from "react-dom";
 
+// let container;
+// beforeEach(()=>{
+//     container=document.createElement("div");
+//     document.body.appendChild(container);
+// })
 
 jest.mock("./api/fetchShow")
 
 test("Render of Episodes", async ()=>{
+// act(() =>{
+//     ReactDOM.render(<App />);
+    mockFetchShow.mockResolvedValueOnce({data: episodeFixture});
 
-   mockFetchShow.mockResolvedValueOnce({data: episodeFixture});
+     const {queryAllByTestId, getByText } = render(<App />);
+     await waitFor(()=>{
+         getByText(/dropdown/i);
+  });   
+     const button = getByText(/dropdown/i);
+     useEvent.click(button);
+     const press = getByText(/Season 3 Suzie/i);
+     userEvent.click(press);
+     expect(queryAllByTestId(/episodes/i)).toHaveLength(1);
+});
 
-    const {getByTestId, queryAllByTestId } = render(<App />);
 
 
 
@@ -23,4 +41,3 @@ test("Render of Episodes", async ()=>{
     
     // })
 
-})
--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -5,15 +5,19 @@ import Episodes from './Episodes';
 
 const episodesFixture = {
     id:2993,
-    url:"http://www.tvmaze.com/shows/2993/stranger-things",name:"Stranger Things",type:"Scripted",language:"English",
+    url:"http://www.tvmaze.com/shows/2993/stranger-things",
+    name:"Season 3 Suzie",
+    type:"Scripted",
+    language:"English",
     genres:["Drama","Fantasy"],
 }
+test("Render episodes component without errors", () =>{
+    render(<Episodes episodes={[]} />);
+})
 test("Episode's content",()=>{
     const { queryAllByTestId, rerender } = render(<Episodes episodes={[]} />);
 
-     expect(queryAllByTestId("episodes")).toHaveLength(0);
-    
-     rerender(<Episodes episodes={[episodesFixture]}/>)
+     rerender(<Episodes episodes={[episodesFixture.id]}/>)
 
      expect(queryAllByTestId("episodes")).toHaveLength(0)
 })
